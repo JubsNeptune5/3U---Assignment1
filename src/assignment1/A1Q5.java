0d27c3387fb28d43e150f673ddedb6b1493c580e
laveh2107 <laveh2107@HRH-IC0027329.wrdsb.ca>
2017-02-10 09:43:45 -0500

    Revised 3 Commit

--- src/assignment1/A1Q5.java
+++ src/assignment1/A1Q5.java
@@ -8,6 +8,8 @@ package assignment1;
 import becker.robots.City;
 import becker.robots.Direction;
 import becker.robots.Robot;
+import becker.robots.Thing;
+import becker.robots.Wall;
 
 /**
  *
@@ -23,10 +25,56 @@ public class A1Q5 {
         City moseisley = new City();
        
         //Create first (0,1) robot
-        Robot han = new Robot(moseisley, 0, 1, Direction.SOUTH);
-        
+        Robot han = new Robot(moseisley, 0, 1, Direction.WEST);
+        han.setLabel("han");
         //Create second (3,3) robot
-        Robot chewy = new Robot(moseisley, 3, 3, Direction.SOUTH);
+        Robot chewy = new Robot(moseisley, 3, 3, Direction.EAST);
+        chewy.setLabel("chewy");
+        
+        //Create walls
+        new Wall(moseisley, 3, 3, Direction.SOUTH);
+        new Wall(moseisley, 3, 3, Direction.EAST);
+        new Wall(moseisley, 2, 3, Direction.EAST);
+        new Wall(moseisley, 2, 3, Direction.NORTH);
+        new Wall(moseisley, 2, 3, Direction.WEST);
+        
+        //Place things
+        new Thing(moseisley, 0, 0);
+        new Thing(moseisley, 1, 0);
+        new Thing(moseisley, 1, 1);
+        new Thing(moseisley, 1, 2);
+        new Thing(moseisley, 2, 2);
+        
+        //han pick up first and second item item and move
+        han.move();
+        han.pickThing();
+        han.turnLeft();
+        han.move();
+        han.pickThing();
+        han.turnLeft();
+        
+        //chewy pick up it's first item
+        chewy.turnLeft();
+        chewy.turnLeft();
+        chewy.move();
+        chewy.turnLeft();
+        chewy.turnLeft();
+        chewy.turnLeft();
+        chewy.move();
+        chewy.pickThing();
+        
+        //Robots meet
+        chewy.move();
+        chewy.turnLeft();
+        han.move();
+        han.pickThing();
+        chewy.pickThing();
+        
+        
+
+        
+        
+        
         
     }
     
